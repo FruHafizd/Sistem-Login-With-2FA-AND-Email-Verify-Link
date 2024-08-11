a3df80fa47fd5c6caf1a5215a395229b5a727900
FruHafizd <muhammadhafidz12333@gmail.com>
2024-08-11 12:12:48 +0800

    Modified Route (Still Have bugs)

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/router/index.php';
 
 /**
  * 
- * new instanse of router
+ * new instance of router
  * 
  */
 $router = new Router();
@@ -21,11 +21,6 @@ $router = new Router();
  * 
  */
 $router->get('/','index.php');
-/**
- * 
- * handle / route
- * 
- */
 $router->get('/home','index.php');
 $router->get('/dashboard','dashboard.php');
 $router->get('/login','login.php');
@@ -40,13 +35,25 @@ $router->get('/ubahBarang','/PengelolaanBarang/update.php');
 $router->get('/emailVerify','/auth/emailVerification.php');
 $router->get('/authenticationApplication','/auth/codeAuthenticator.php');
 
-$email = $_SESSION['auth_user']['email']; // Ambil email dari session
-$token = $_SESSION['auth_user']['verify_token']; 
-$router->get("/profile?id=" . $email, 'userProfile.php');
+// Pengecekan session sebelum mengaksesnya
+if (isset($_SESSION['auth_user'])) {
+    $email = $_SESSION['auth_user']['email']; // Ambil email dari session
+    $token = $_SESSION['auth_user']['verify_token'];
+
+    $router->get("/profile?id=" . $email, 'userProfile.php');
+    $router->get('/changePassword?token=' . $token . '&email=' . $email, '/passwordReset/password-change.php');
+} else {
+    // Jika session belum di-set, arahkan ke halaman login
+    $router->get("/profile", function() {
+        header("Location: /login");
+        exit();
+    });
+
+    $router->get("/changePassword", function() {
+        header("Location: /login");
+        exit();
+    });
+}
 
 $router->get('/resetPassword','/passwordReset/password-reset.php');
-$router->get('/changePassword?token='.$token.'&email='.$email,'/passwordReset/password-change.php');
-
-
-
 
--- views/register.php
+++ views/register.php
@@ -1,5 +1,4 @@
 <?php 
-session_start();
 include('template/header.php');
 
 ?>
